b2a2e44c1149535eb981c86a4e90d6a7e33f6702
morpheusos <eulenberg.tim@gmail.com>
2024-03-24 05:53:00 +0100

    input gets collected

--- stuff_app/pages/1_🎰_Random_selection_machine.py
+++ stuff_app/pages/1_🎰_Random_selection_machine.py
@@ -1,3 +1,4 @@
+import cachetools
 import streamlit as st
 
 st.set_page_config(page_title="Random Selection Machine", page_icon=":slot_machine:")
@@ -6,8 +7,23 @@ st.sidebar.header("Random Selection Machine")
 
 st.header("Random Selection Machine")
 
+if 'elements' not in st.session_state:
+    st.session_state.elements = []
 
-st.header("Test text")
+
+box = st.container()
+
+def adder():
+    st.session_state.elements.append(st.session_state.element_to_add)
 
 
-# TODO: add backstory
+
+with box:
+    element = st.chat_input("Enter the items to be picked here...", key='element_to_add', on_submit=adder)
+
+
+print(st.session_state.elements)
+for item in st.session_state.elements:
+    st.sidebar.write(item)
+
+
