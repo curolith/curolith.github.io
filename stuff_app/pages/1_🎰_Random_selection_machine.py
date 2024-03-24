6e1582e04279c999d7b855d02b5b688b78cc8957
morpheusos <eulenberg.tim@gmail.com>
2024-03-24 21:28:34 +0100

    refactor

--- stuff_app/pages/1_🎰_Random_selection_machine.py
+++ stuff_app/pages/1_🎰_Random_selection_machine.py
@@ -1,8 +1,6 @@
 import random
 
-import cachetools
 import streamlit as st
-import pyperclip
 
 st.set_page_config(page_title="Random Selection Machine", page_icon=":slot_machine:")
 
@@ -35,15 +33,17 @@ st.session_state.selection = None
 
 def key_gen():
     i = st.session_state.key_gen
-    st.session_state.key_gen = i+1
+    st.session_state.key_gen = i + 1
     return f'generated_key_{i}'
+
 
 def adder():
     st.session_state.elements.append(st.session_state.element_to_add)
 
-def selecter(item):
+
+def selecter(item_inner):
     if st.session_state.remover_active == 1:
-        st.session_state.selection = item
+        st.session_state.selection = item_inner
 
 
 def toggle_remover():
@@ -57,11 +57,11 @@ def even_dist(n, i):
     c_box = 0
     while len(elements) > 0 and len(boxes[c_box]) < i:
         print(len(elements), len(boxes[c_box]), print(c_box))
-        boxes[c_box].append(elements.pop(random.randint(0, len(elements)-1)))
-        if c_box == n-1:
+        boxes[c_box].append(elements.pop(random.randint(0, len(elements) - 1)))
+        if c_box == n - 1:
             c_box = 0
         else:
-            c_box = c_box+1
+            c_box = c_box + 1
     st.session_state.distribution = boxes
     if len(elements) > 0:
         st.session_state.remaining = elements
@@ -88,8 +88,6 @@ def fill(n, i):
         st.session_state.remaining = []
 
 
-
-
 def random_selection(n, i, d):
     if len(st.session_state.elements) < 1:
         return
@@ -99,16 +97,8 @@ def random_selection(n, i, d):
         fill(n, i)
 
 
-
-
-
-
-
-
-
 with box:
     element = st.chat_input("Enter the items to be picked here...", key='element_to_add', on_submit=adder)
-
 
 print(st.session_state.elements)
 
@@ -131,7 +121,8 @@ with st.container():
 
         with col3:
             d_even_dist = st.toggle("Distribute evenly")
-    st.button("Randomize", on_click=random_selection(n_elements, i_size, d_even_dist), type='primary')
+    if len(st.session_state.elements) > 0:
+        st.button("Randomize", on_click=random_selection(n_elements, i_size, d_even_dist), type='primary')
 
 print(n_elements, i_size, d_even_dist)
 
@@ -139,8 +130,7 @@ if len(st.session_state.distribution) > 0:
     st.divider()
 
 with st.container():
-
-    tripple_dist = [[],[],[]]
+    tripple_dist = [[], [], []]
 
     boxes = st.session_state.distribution.copy()
 
@@ -154,12 +144,11 @@ with st.container():
                 tripple_dist[iter_index].append(['Remaining', st.session_state.remaining])
         else:
             tripple_dist[iter_index].append([f"Group {counter}", boxes.pop(0)])
-            counter = counter+1
+            counter = counter + 1
             if iter_index == 2:
                 iter_index = 0
             else:
-                iter_index = iter_index+1
-
+                iter_index = iter_index + 1
 
     left, center, right = st.columns(3)
 
@@ -170,14 +159,12 @@ with st.container():
                 for elitem in element_map[1]:
                     st.button(elitem, key=key_gen())
 
-
     with center:
         for element_map in tripple_dist[1]:
             with st.container():
                 st.write(element_map[0])
                 for elitem in element_map[1]:
                     st.button(elitem, key=key_gen())
-
 
     with right:
         for element_map in tripple_dist[2]:
@@ -186,12 +173,7 @@ with st.container():
                 for elitem in element_map[1]:
                     st.button(elitem, key=key_gen())
 
-
-
 if len(st.session_state.distribution) > 0:
     st.divider()
-    st.write("Click the icon on the right to copy the selection:")
+    st.write("Hover and click the icon on the right to copy the selection:")
     st.code(st.session_state.distribution + st.session_state.remaining)
-
-
-
